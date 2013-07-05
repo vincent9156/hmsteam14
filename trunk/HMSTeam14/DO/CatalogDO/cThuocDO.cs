1203bd17308c6ac7375ba7b8fe038a825562a39a
hieu48.chip@gmail.com <hieu48.chip@gmail.com@d8784a5e-57a1-c1f4-44bf-0d189bfe7613>
2013-07-05 07:27:51 +0000

    #7 add medicine

---
+++ trunk/HMSTeam14/DO/CatalogDO/cThuocDO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DO
+{
+    public class cThuocDO
+    {
+        // khai bao bien su dung
+        public string MATHUOC { get; set; }
+        public string TENTHUONGMAI { get; set; }
+        public string TENBIETDUOC { get; set; }
+        public string CACHDUNG { set; get; }
+        public string DONVIDUNG { set; get; }
+        public string DONVI { set; get; }
+        public string GHICHU { set; get; }
+        public Boolean TRANGTHAI { set; get; }
+        public DateTime NGAYTAO { set; get; }
+    }
+}
